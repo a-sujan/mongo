d88a44e170dd2dc2736d6ab641b8b75b1f998eb8
sujant <mesujant@gmail.com>
2024-04-11 16:06:13 +0000

    expose port

--- init_replica.js
+++ init_replica.js
@@ -1,9 +1,8 @@
 rs.initiate({
-  _id: "sujant-replica-test",  // Replica set name
+  _id: "replica-test",  // Replica set name
   members: [
     { _id: 0, host: "replica-1" }, // Replace with the hostname and port of the first member
     { _id: 1, host: "replica-2" }, // Replace with the hostname and port of the second member
-    { _id: 2, host: "replica-3" }, // Replace with the hostname and port of the second member
-    // Add more members as needed
+    { _id: 2, host: "replica-3" } // Replace with the hostname and port of the second member
   ]
-})
+});
