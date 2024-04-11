b6f0f440adbd94eeb302aa4f2cab07dd413b4a08
sujant <mesujant@gmail.com>
2024-04-11 15:26:15 +0000

    change image source

--- setup-replica.sh
+++ setup-replica.sh
@@ -3,12 +3,12 @@
 docker network create replica-test
 # setup required config to enable replicaset
 
-docker run -d --name replica-1 --network replica-test -v ./config/mongod.conf:/etc/mongod.conf  gitlab.wlink.com.np:4567/tamang.sujan/mongo:4.4.0.1 
+docker run -d --name replica-1 --network replica-test -v ./config/mongod.conf:/etc/mongod.conf  sujant/mongod4.4.0:1.0.0 
 
 
-docker run -d --name replica-2 --network replica-test -v ./config/mongod.conf:/etc/mongod.conf  gitlab.wlink.com.np:4567/tamang.sujan/mongo:4.4.0.1
+docker run -d --name replica-2 --network replica-test -v ./config/mongod.conf:/etc/mongod.conf  sujant/mongod4.4.0:1.0.0
 
-docker run -d --name replica-3 --network replica-test -v ./config/mongod.conf:/etc/mongod.conf  gitlab.wlink.com.np:4567/tamang.sujan/mongo:4.4.0.1
+docker run -d --name replica-3 --network replica-test -v ./config/mongod.conf:/etc/mongod.conf  sujant/mongod4.4.0:1.0.0
 # compose up 3 containers
 
 
